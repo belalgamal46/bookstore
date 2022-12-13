ae6c97c69b41c88bd6f02463db3023f9708b7529
belalgamal46 <belalgamal00@gmail.com>
2022-12-13 14:52:01 +0200

    Add onClick to the categories to check status

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,25 +1,41 @@
 import './styles/navbar.css';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories';
 import navLinks from '../../fixtures/navLinks';
 
-const Navbar = () => (
-  <>
-    <header>
-      <nav>
-        <h1>Bookstore CMS</h1>
-        <ul>
-          {navLinks.map(({ text, url }) => (
-            <li key={url}>
-              <Link to={url}>{text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-    <main>
-      <Outlet />
-    </main>
-  </>
-);
+const Navbar = () => {
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>Bookstore CMS</h1>
+          <ul className="nav-links">
+            {navLinks.map(({ text, url }) => (
+              <li key={url}>
+                <Link
+                  to={url}
+                  onClick={text === 'CATEGORIES' ? handleClick : ''}
+                  className={`${pathname === url ? 'active' : ''} nav-link`}
+                >
+                  {text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default Navbar;
