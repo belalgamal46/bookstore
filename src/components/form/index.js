88bc12e0e9d48396e0aa6165a45d11775d4a0e33
belalgamal46 <belalgamal00@gmail.com>
2022-12-13 14:11:19 +0200

    Implement add book functionality

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,14 +1,53 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books';
 import './styles/form.css';
 
-const Form = () => (
-  <div>
-    <h4>ADD NEW BOOK</h4>
-    <form>
-      <input type="text" placeholder="Enter Book Title" />
-      <input type="text" placeholder="Enter Author Name" />
-      <button type="submit">ADD BOOK</button>
+const Form = () => {
+  const dispatch = useDispatch();
+  const [bookTitle, setBookTitle] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const book = { bookTitle, bookAuthor, id: uuidv4() };
+    dispatch(addBook(book));
+    setBookTitle('');
+    setBookAuthor('');
+  };
+
+  const handleChange = (event) => {
+    if (event.target.name === 'book-title') {
+      setBookTitle(event.target.value);
+    }
+    if (event.target.name === 'book-author') {
+      setBookAuthor(event.target.value);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>ADD NEW BOOK</h4>
+      <div className="form-inputs">
+        <input
+          type="text"
+          placeholder="Enter Book Title"
+          name="book-title"
+          onChange={handleChange}
+          value={bookTitle}
+        />
+        <input
+          type="text"
+          placeholder="Enter Author Name"
+          name="book-author"
+          onChange={handleChange}
+          value={bookAuthor}
+        />
+        <button type="submit">ADD BOOK</button>
+      </div>
     </form>
-  </div>
-);
+  );
+};
 
 export default Form;
